52e63756e0522491ac0a239582e1a9a7322f7a9e
mdarslan7 <mdarslan7@gmail.com>
2023-07-29 02:14:34 +0530

    added code in Functions

--- Functions _Methods in Java/src/Main.java
+++ Functions _Methods in Java/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        String personalized = greet(name);
+        System.out.println(personalized);
+    }
+
+    static String greet(String name) {
+        return "Hello " + name + "!";
     }
 }
