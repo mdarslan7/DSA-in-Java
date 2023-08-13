db5f2b32b626badae811918f92a7ccae7bc9eabe
mdarslan7 <mdarslan7@gmail.com>
2023-08-14 00:46:09 +0530

    added code in Functions

--- Functions _Methods in Java/src/AssignmentQ10.java
+++ Functions _Methods in Java/src/AssignmentQ10.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class AssignmentQ10 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is palindrome: ");
+        int userInput = sc.nextInt();
+        boolean answer = isPalindrome(userInput);
+        if(answer)
+            System.out.println("It's Palindrome!");
+        else
+            System.out.println("It's not Palindrome!");
+    }
 
+    static boolean isPalindrome(int userInput) {
+        int num = userInput;
+        int n, rev=0;
+        while(num > 0) {
+            n = num % 10;
+            rev = (rev*10) + n;
+            num /= 10;
+        }
+        return userInput == rev;
     }
 }
