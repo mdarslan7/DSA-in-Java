8fb9202f53319aa97474283b1b2a9ceaaca7b6cf
mdarslan7 <mdarslan7@gmail.com>
2023-10-17 01:35:30 +0530

    added InputArray.java

--- Arrays_and_ArrayList/src/MultidimensionalArray.java
+++ Arrays_and_ArrayList/src/MultidimensionalArray.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MultidimensionalArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[][] arr = new int[3][3];
-        System.out.println(arr[0].length);
+
+//        Input
+        for(int row=0; row< arr.length; row++) {
+            for(int col=0; col<arr[row].length; col++) {
+                arr[row][col] = sc.nextInt();
+            }
+        }
+
+//        Output using nested loop
+//        for(int row=0; row<arr.length; row++) {
+//            for(int col=0; col<arr[row].length; col++) {
+//                System.out.print(arr[row][col] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        Output using toString()
+//        for(int row=0; row< arr.length; row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//        }
+
+//        Output using enhanced for loop
+        for(int[] a: arr) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 }
