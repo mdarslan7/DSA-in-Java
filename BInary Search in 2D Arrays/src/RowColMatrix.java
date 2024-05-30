a9adf268c116b5be3906a1e11cd25fa3b6b118c5
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-05-31 03:15:24 +0530

    add misc files

--- BInary Search in 2D Arrays/src/RowColMatrix.java
+++ BInary Search in 2D Arrays/src/RowColMatrix.java
@@ -2,13 +2,11 @@ import java.util.Arrays;
 public class RowColMatrix {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
+                {1, 1},
+                {2, 2}
         };
 
-        int target = 11;
+        int target = 0;
 
         int r = 0;
         int c = matrix[0].length - 1;
