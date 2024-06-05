c3c248c9762568890e93072dcc6975aa1baa624e
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-06-05 20:49:21 +0530

    add bubbleSort

---
+++ Bubble Sort Algorithm/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 4 ,3, 2, 1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr) {
+        boolean swapped;
+        int n = arr.length;
+        //outer loop
+        for(int i = 0; i < n; i++) {
+            swapped = false;
+            //inner loop for comparisons
+            for(int j = 1; j < n-i; j++) {
+                //swapping when condition met
+                if(arr[j-1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+
+            //if elements do not get swapped for a particular value of i, it means
+            //that the array is now sorted hence we can exit the program
+            if(!swapped) {
+                break;
+            }
+        }
+    }
+}
