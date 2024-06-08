5970b36709c02c0d4cebdf303bab3caca565aa1a
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-06-08 16:56:59 +0530

    add MissingNumber.java

---
+++ Cyclic Sort Algorithm/src/MissingNumber.java
@@ -0,0 +1,38 @@
+//Amazon
+//Given an array nums containing n distinct numbers in the range [0, n],
+// return the only number in the range that is missing from the array.
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {4, 0, 2, 1};
+        System.out.println(missingNumber(nums));
+    }
+    static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+
+        while(i < n) {
+            int correctIndex = nums[i];
+
+            if(correctIndex == n) {
+                i++;
+                continue;
+            }
+
+            if(nums[correctIndex] != nums[i]) {
+                int temp = nums[correctIndex];
+                nums[correctIndex] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        for (i=0; i<nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
