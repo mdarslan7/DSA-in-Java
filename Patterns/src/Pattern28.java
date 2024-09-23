aeb9ca572223daed1c6d7c51c484d758aa7e2efc
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-09-23 13:45:56 +0530

    add pattern-23

--- Patterns/src/Pattern28.java
+++ Patterns/src/Pattern28.java
@@ -6,6 +6,14 @@ public class Pattern28 {
     static void pattern (int n) {
         for (int row = 1; row <= 2 * n - 1; row++) {
             int totalColsInRow = row > n ? 2 * n - row: row;
+            int totalSpaces = n - totalColsInRow;
+
+            //for spaces
+            for(int k=1; k<=totalSpaces; k++) {
+                System.out.print(" ");
+            }
+
+            //for stars
             for (int col = 1; col <= totalColsInRow; col++) {
                 System.out.print("* ");
             }
