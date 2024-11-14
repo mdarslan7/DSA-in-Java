fa5e38374aa42620fd1b506dce025300e0a2718e
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2024-11-14 16:54:12 +0530

    add recursive binary search

--- Recursion/src/BinarySearch.java
+++ Recursion/src/BinarySearch.java
@@ -1,7 +1,22 @@
 public class BinarySearch {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 45, 67, 76};
+        int target = 67;
+        System.out.println(recursiveBinarySearch(arr, target, 0, arr.length-1));
     }
 
+    static int recursiveBinarySearch(int[] arr, int target, int s, int e) {
+        if(s > e)
+            return -1;
 
+        int mid = s + (e - s) / 2;
+
+        if(target == arr[mid])
+            return mid;
+
+        if(target < arr[mid])
+            return recursiveBinarySearch(arr, target, s, mid-1);
+
+        return recursiveBinarySearch(arr, target, mid+1, e);
+    }
 }
