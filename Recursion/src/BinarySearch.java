95726d158fff884f5d07fecca8b605a305d97a65
mdarslan7 <mdarslan7@gmail.com>
2025-01-16 12:53:30 +0530

    verify credentials access

--- Recursion/src/BinarySearch.java
+++ Recursion/src/BinarySearch.java
@@ -1,6 +1,6 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int target = 1;
         System.out.println(recursiveBinarySearch(arr, target, 0, arr.length-1));
     }
