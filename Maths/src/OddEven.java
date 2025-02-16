22437dce578ab79a3a359dc352b10922943c9324
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2025-02-16 18:57:53 +0530

    add OddEven.java

---
+++ Maths/src/OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven {
+    static boolean isOdd(int n) {
+        return (n & 1) == 1;
+    }
+    public static void main(String[] args) {
+        int n = 67;
+        System.out.println(isOdd(n));
+    }
+}
