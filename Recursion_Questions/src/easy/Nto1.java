481f5c16f13cd997af5e51055923f7cb65e31491
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2025-03-18 23:54:17 +0530

    add Nto1.java

---
+++ Recursion_Questions/src/easy/Nto1.java
@@ -0,0 +1,31 @@
+package easy;
+public class Nto1 {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+
+    private static void fun(int n) {
+        if(n == 0)
+            return;
+
+        System.out.println(n);
+        fun(n - 1);
+    }
+
+    private static void funRev(int n) {
+        if(n == 0)
+            return;
+
+        funRev(n - 1);
+        System.out.println(n);
+    }
+
+    private static void funBoth(int n) {
+        if(n == 0)
+            return;
+
+        System.out.println(n);
+        funBoth(n - 1);
+        System.out.println(n);
+    }
+}
