c8eed4432b9773dc7886260b166fb507f47d3bc4
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2025-03-19 00:10:18 +0530

    add sumOfDigits.java

---
+++ Recursion_Questions/src/easy/SumOfDigits.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(1342));
+    }
+
+    private static int sumOfDigits(int n) {
+        if(n == 0)
+            return 0;
+
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+}
