6e55f9d1f4d6a3cd39a6d3b081df6152c53da556
mdarslan7 <mdarslan7@gmail.com>
2025-04-03 02:24:51 +0530

    add subSet

--- Recursion_Questions/src/strings/SubSet.java
+++ Recursion_Questions/src/strings/SubSet.java
@@ -1,12 +1,13 @@
 package strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        List<List<Integer>> ans = subSet(arr);
+        List<List<Integer>> ans = subSetDuplicates(arr);
         for(List<Integer> list : ans) {
             System.out.println(list);
         }
@@ -27,4 +28,29 @@ public class SubSet {
 
         return outer;
     }
+
+    static List<List<Integer>> subSetDuplicates(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<arr.length; i++) {
+            start = 0;
+            if(i>0 && arr[i] == arr[i-1])
+                start = end + 1;
+            end = outer.size() - 1;
+            int n = outer.size();
+            for(int j=start; j<n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+
+
 }
