d91487ce73b987d24a0695d3e8830991425ee034
mdarslan7 <mdarslan7@gmail.com>
2025-04-02 20:07:23 +0530

    add Stream.java

---
+++ Recursion_Questions/src/strings/Stream.java
@@ -0,0 +1,35 @@
+package strings;
+
+public class Stream {
+    public static void main(String[] args) {
+        skip("baccadh", "");
+        System.out.println(skip("baccadhewawae"));
+    }
+
+    static void skip(String str, String ans) {
+        if(str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = str.charAt(0);
+
+        if(ch == 'a')
+            skip(str.substring(1), ans);
+        else
+            skip(str.substring(1), ans + ch);
+    }
+
+    static String skip(String str) {
+        if(str.isEmpty()) {
+            return "";
+        }
+
+        char ch = str.charAt(0);
+
+        if(ch == 'a')
+            return skip(str.substring(1));
+        else
+            return ch + skip(str.substring(1));
+    }
+}
