eb7e71cc3fa639eaa66a4dfd7fc56f82823f3804
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2025-07-08 20:48:06 +0530

    add in-built queue example

--- Stacks and Queues/src/InBuiltExamples.java
+++ Stacks and Queues/src/InBuiltExamples.java
@@ -1,18 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class InBuiltExamples {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(23);
-        stack.push(34);
-        stack.push(64);
-        stack.push(24);
-        stack.push(87);
+        //Stack
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(23);
+//        stack.push(34);
+//        stack.push(64);
+//        stack.push(24);
+//        stack.push(87);
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        //Queue
+        Queue<Integer> q = new LinkedList<>();
+        q.add(23);
+        q.add(53);
+        q.add(65);
+        q.add(67);
+        q.add(98);
+
+        System.out.println(q.peek());
+
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
