4288210224623a77c043f13948e4e07285e55577
Md Arslan <93745288+mdarslan7@users.noreply.github.com>
2025-07-08 23:29:21 +0530

    add CustomStack.java

---
+++ Stacks and Queues/src/CustomStack.java
@@ -0,0 +1,44 @@
+public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    int ptr = -1;
+
+    public CustomStack() {
+        this(DEFAULT_SIZE);         //calling parameterized constructor
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return ptr == data.length - 1;
+    }
+
+    public boolean isEmpty() {
+        return ptr == -1;
+    }
+
+    public boolean push(int item) {
+        if(isFull())
+            return false;
+
+        ptr += 1;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws Exception {
+        if(isEmpty())
+            throw new Exception("Cannot pop from an empty stack!");
+
+        return data[ptr--];
+    }
+
+    public int peek() throws Exception {
+        if(isEmpty())
+            throw new Exception("Cannot pop from an empty stack!");
+
+        return data[ptr];
+    }
+}
